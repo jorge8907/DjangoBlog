290b26073fd636575c72040ca590ef0e4cac0270
Jorge Candamil <jorl.8907@gmail.com>
2022-06-06 23:18:53 -0500

    Funcionalidad ecommerce con autenticacion

---
+++ cart/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from products.models import Product
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+
+ 
+
+# Create your models here.
+class CartItem(models.Model):
+    amount = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    
---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+        
+class User(AbstractUser):
+    isEmailValid = models.BooleanField(default=False)
+    country = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    district = models.CharField(max_length=50, null=True)
+    zipcode = models.IntegerField(null=True)
+    
